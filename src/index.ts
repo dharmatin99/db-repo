d892d7940163dda99ca069bd9bf1ce271e98826c
dharmatin99 <deni.dharmatin@99.co>
2020-09-20 13:52:11 +0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import "reflect-metadata";
+import {createConnection, getConnectionOptions} from "typeorm";
+
+export const connection = async(config) => {
+    const connectionOptions = await getConnectionOptions();
+    return await createConnection({ ...config, ...connectionOptions });
+}
+
